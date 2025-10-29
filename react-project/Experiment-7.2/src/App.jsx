0cbe906b00798f233a9a8efa1216c8bb314a39f7
AryanSingh1112 <singharyan120505@gmail.com>
2025-10-29 18:24:46 +0530

    done

--- react-project/Experiment-7.2/src/App.jsx
+++ react-project/Experiment-7.2/src/App.jsx
@@ -1,11 +1,10 @@
-import "./App.css"; // make sure styles are applied
-// Trigger rebuild: import path is correct, should be ./components/LibraryManagement
+import "./App.css";
 import LibraryManagement from "./components/LibraryManagement";
 
 function App() {
   return (
     <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px" }}>
-     <LibraryManagement />
+      <LibraryManagement />
     </div>
   );
 }
