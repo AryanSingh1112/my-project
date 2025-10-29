9142f9d2fbe84879910d31c4682a48ba7ba6e4a4
AryanSingh1112 <singharyan120505@gmail.com>
2025-10-29 19:03:25 +0530

    fix: clean up App.jsx for fresh 7.2

---
+++ react-project/Experiment-7.2/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";import "./App.css";
+
+import LibraryManagement from "./components/LibraryManagement";import LibraryManagement from "./components/LibraryManagement";
+
+
+
+function App() {function App() {
+
+  return (  return (
+
+    <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px" }}>    <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px" }}>
+
+      <LibraryManagement />      <LibraryManagement />
+
+    </div>    </div>
+
+  );  );
+
+}}
+
+
+
+export default App;export default App;
