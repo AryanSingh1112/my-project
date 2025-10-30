371719e58ea90c0e56b6aba292a6bdcf6d06c500
AryanSingh1112 <singharyan120505@gmail.com>
2025-10-30 11:04:58 +0530

    Recreate Experiment-7.2 with working React app, Dockerfile, and build context

--- react-project/Experiment-7.2/src/App.jsx
+++ react-project/Experiment-7.2/src/App.jsx
@@ -3,9 +3,12 @@ import LibraryManagement from "./components/LibraryManagement";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px" }}>
+    <div style={{ backgroundColor: "black", minHeight: "100vh", padding: "20px" }}>
       <LibraryManagement />
+      Hi there! Welcome to Experiment 7.2
     </div>
+
+   
   );
 }
 
